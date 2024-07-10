1801c2c324421084789465e2b92ee40365dacd47
kalan <kalansoft@gmail.com>
2024-07-10 15:26:16 +0700

    feat: chain listener

---
+++ src/chain.rs
@@ -0,0 +1,109 @@
+use subxt::{
+    config::{
+        substrate::{BlakeTwo256, SubstrateHeader},
+        SubstrateExtrinsicParams,
+    },
+    events::StaticEvent,
+    utils::{AccountId32, MultiAddress, MultiSignature, H256},
+    Config, OnlineClient,
+};
+use thiserror::Error;
+use tokio::sync::broadcast;
+use tokio_util::sync::CancellationToken;
+
+use crate::Result;
+
+#[subxt::subxt(runtime_metadata_path = "metadata.scale")]
+pub mod airo {}
+
+pub enum AiroConfig {}
+
+impl Config for AiroConfig {
+    type Hash = H256;
+    type AccountId = AccountId32;
+    type Address = MultiAddress<Self::AccountId, ()>;
+    type Signature = MultiSignature;
+    type Hasher = BlakeTwo256;
+    type Header = SubstrateHeader<u32, BlakeTwo256>;
+    type ExtrinsicParams = SubstrateExtrinsicParams<Self>;
+    type AssetId = u32;
+}
+
+type AiroClient = OnlineClient<AiroConfig>;
+type Block = subxt::blocks::Block<AiroConfig, AiroClient>;
+type OrderId = u32;
+type ModelId = String;
+
+#[derive(Clone, Debug)]
+pub enum ChainEvent {
+    /// A new order has been created.
+    OrderCreated {
+        /// The order ID.
+        order_id: OrderId,
+        /// The model ID.
+        model_id: ModelId,
+    },
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Failed to get the next block")]
+    NextBlock,
+}
+
+pub struct ChainClient {
+    pub client: AiroClient,
+}
+
+impl ChainClient {
+    pub async fn new(url: &str) -> Result<Self> {
+        // TODO. It might make sense to reconnect automatically
+        // https://github.com/paritytech/subxt/blob/master/subxt/examples/setup_reconnecting_rpc_client.rs
+        let client = AiroClient::from_insecure_url(url).await?;
+        tracing::info!("Connected to airo node at {}", url);
+        Ok(Self { client })
+    }
+
+    pub async fn listen(
+        &self,
+        token: CancellationToken,
+        sender: broadcast::Sender<ChainEvent>,
+    ) -> Result<()> {
+        let mut blocks_sub = self.client.blocks().subscribe_finalized().await?;
+        while let Some(block) = blocks_sub.next().await {
+            tokio::select! {
+                _ = token.cancelled() => return Ok(()),
+                result = self.process_block(block?, &sender) => result?,
+            }
+        }
+
+        tracing::error!("Failed to get the next block");
+        Err(Error::NextBlock.into())
+    }
+
+    async fn process_block(
+        &self,
+        block: Block,
+        sender: &broadcast::Sender<ChainEvent>,
+    ) -> Result<()> {
+        let events = block.events().await?;
+        for event in events.iter() {
+            let event = event?;
+            let event_meta = event.event_metadata();
+            let pallet_name = event_meta.pallet.name();
+            let event_name = event_meta.variant.name.as_str();
+
+            if airo::airo_market::events::OrderCreated::is_event(pallet_name, event_name) {
+                if let Some(order_created) =
+                    event.as_event::<airo::airo_market::events::OrderCreated>()?
+                {
+                    let order_id = order_created.order_id;
+                    let model_id = String::from_utf8_lossy(&order_created.model_id.0).into_owned();
+                    sender.send(ChainEvent::OrderCreated { order_id, model_id })?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,24 @@
+//! TODO. Write docs.
+
 #![warn(missing_docs)]
 
+use axum::Router;
 use core::{
     future::Future,
     net::{Ipv4Addr, SocketAddr},
 };
-use std::error::Error;
-
-use axum::Router;
-use once_cell::sync::Lazy;
-use tokio::{
-    net::TcpListener,
-    signal,
-    sync::{broadcast, watch},
-    task::JoinHandle,
-};
+use std::{error::Error, sync::Arc};
+use tokio::{net::TcpListener, signal, sync::broadcast};
 use tokio_util::{sync::CancellationToken, task::TaskTracker};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
-use crate::{config::Config};
+use crate::{chain::ChainClient, config::Config};
 
+mod chain;
 mod check;
 mod config;
-
-static CONFIG: Lazy<Config> = Lazy::new(Config::new);
 
 #[derive(OpenApi)]
 #[openapi(paths(check::health))]
@@ -32,6 +26,7 @@ struct ApiDoc;
 
 type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
+/// Spawns a critical task. If the task fails, the given token is cancelled.
 async fn critical_task<F>(name: &str, token: CancellationToken, task: F) -> Result<()>
 where
     F: Future<Output = Result<()>> + Send + 'static,
@@ -44,25 +39,54 @@ where
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    let config = Config::new();
     tracing_subscriber::registry().with(tracing_subscriber::fmt::layer()).init();
 
     let tracker = TaskTracker::new();
     let token = CancellationToken::new();
 
-    tracker.spawn(critical_task("http_server", token.clone(), run_http_server(token.clone())));
+    // A channel for receiving chain events produced by the chain listener.
+    let (chain_tx, _chain_rx) = broadcast::channel(16);
+
+    // Start the chain client.
+    let chain_client = {
+        let chain_client = ChainClient::new(&config.airo_node).await.map_err(|e| {
+            tracing::error!("Failed to connect to airo node: {}", e);
+            e
+        })?;
+        Arc::new(chain_client)
+    };
+
+    // Spawn the chain listener.
+    {
+        let chain_client = chain_client.clone();
+        let token = token.clone();
+        tracker.spawn(critical_task("chain_listener", token.clone(), async move {
+            chain_client.listen(token, chain_tx).await
+        }));
+    }
+
+    // Spawn the HTTP server.
+    tracker.spawn(critical_task(
+        "http_server",
+        token.clone(),
+        http_server(token.clone(), config.port),
+    ));
+
     tracker.spawn(shutdown_signal(token));
 
     tracker.close();
     tracker.wait().await;
+    Ok(())
 }
 
-async fn run_http_server(token: CancellationToken) -> Result<()> {
+async fn http_server(token: CancellationToken, port: u16) -> Result<()> {
     let app = Router::new()
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .merge(check::routes());
 
-    let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, CONFIG.port));
+    let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, port));
     let listener = TcpListener::bind(&address).await.unwrap();
 
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
@@ -91,11 +115,12 @@ async fn shutdown_signal(token: CancellationToken) {
     tokio::select! {
         _ = ctrl_c => {
             tracing::info!("Received shutdown signal");
+            token.cancel();
         },
         _ = terminate => {
             tracing::info!("Received termination signal");
+            token.cancel();
         },
+        _ = token.cancelled() => {},
     }
-
-    token.cancel();
 }
