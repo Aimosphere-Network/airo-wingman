550996b4dcbd9fcf38dbada34b45ba24f6822c69
Giulia <mulan@Giulias-MacBook-Air.local>
2024-08-28 19:18:00 -0300

    refactor: making prediction loop explicit for key management later

--- .maintain/cog/FHEModels/predict.py
+++ .maintain/cog/FHEModels/predict.py
@@ -3,37 +3,61 @@ import pandas as pd
 import concrete.ml.sklearn as concrete
 from cog import BasePredictor, Input, Path
 
+model_file = "model.pkl"
+train_data_file = "train_data.csv"
+
 class Predictor(BasePredictor):
     def setup(self):
-        # Load model
-        with open("model.pkl", "rb") as file:
-            model = joblib.load(file)   
+        # Load sklearn model
+        sklearn_model = joblib.load(model_file)   
         # Load training data
-        with open("train_data.csv", 'rb') as file: 
-            train_data = joblib.load(file)
+        train_data = joblib.load(train_data_file)
         
         # Convert model into concrete model
-        self.model = concrete.DecisionTreeClassifier.from_sklearn_model(model, n_bits=6)
-
-        # Generate circuit from trained model, using training data as shape
-        self.circuit = self.model.compile(train_data)
+        self.concrete_model = concrete.DecisionTreeClassifier.from_sklearn_model(sklearn_model, n_bits=6)
         
-        # Generate keys for circuit
-        self.circuit.client.keygen(force=True)
-
+        # Generate circuit from trained model, using training data as shape
+        self.fhe_circuit = self.concrete_model.compile(train_data)
+            
+        # Generate keys for circuit, force new keygen everytime this runs
+        self.fhe_circuit.keygen(force=True)
 
     def predict(
         self,
-        request: Path = Input(description="pkl file containing features"),
+        path: Path = Input(description="csv file containing features"),
         ) -> str:
-        # Load the features from the .pkl file
-        df = pd.read_pickle(request)
 
-        # Predict for each request
-        predictions = pd.DataFrame()
-        for row in df.itertuples(index=True):
-            p = self.circuit.run(row)
-            df = pd.concat([df, p[0]], ignore_index=True)
-        
-        return predictions
+        # Load features from .csv file
+        requests = joblib.load(path)
+
+        for req in requests:
+            # Quantize input (float)
+            q_req = self.concrete_model.quantize_input([req])
+            
+            # Encrypt the input
+            q_req_enc = self.fhe_circuit.encrypt(q_req)
+
+            # Execute the linear product in FHE (run circuit for prediction)
+            q_result_enc = self.fhe_circuit.run(q_req_enc)
+
+            # Decrypt result (integer)
+            q_result = self.fhe_circuit.decrypt(q_result_enc)
+
+            # De-quantize result
+            result = self.concrete_model.dequantize_output(q_result)
+
+            print(result)
+
+        #     # Apply either the sigmoid if it is a binary classification task, which is the case in this 
+        #     # example, or a softmax function in order to get the probabilities (in the clear)
+        #     y_proba = model.post_processing(y)
+
+        #     # Since this model does classification, apply the argmax to get the class predictions (in the clear)
+        #     # Note that regression models won't need the following line
+        #     y_class = numpy.argmax(y_proba, axis=1)
+
+        #     y_pred_fhe_step += list(y_class)
+
+        # y_pred_fhe_step = numpy.array(y_pred_fhe_step)
+
 
